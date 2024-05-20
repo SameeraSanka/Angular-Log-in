65e10fbb3dc45cb89a46f9a875cf7b209bb4f274
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-05-20 11:27:31 +0530

    parsialy complete

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service'; 
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatInputModule, MatIconModule, ReactiveFormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit  {
+  authService = inject(AuthService)
+  hide=true;
+  form!: FormGroup;
+  fb = inject(FormBuilder);
+
+  login()
+  {
+    this.authService.login(this.form.value).subscribe((response)=>{
+      console.log(response);
+    })
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required]]
+    });
+  }
+}
